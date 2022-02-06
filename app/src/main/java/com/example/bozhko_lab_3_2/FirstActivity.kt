72db1846f7e6b555450cdc11e44937aaea00b7bd
BozhkoAV <avbozhko@mail.ru>
2022-02-06 16:45:58 +0300

    Second attempt to make Navigation Drawer

--- app/src/main/java/com/example/bozhko_lab_3_2/FirstActivity.kt
+++ app/src/main/java/com/example/bozhko_lab_3_2/FirstActivity.kt
@@ -1,20 +1,11 @@
 package com.example.bozhko_lab_3_2
 
 import android.content.Intent
-<<<<<<< Updated upstream
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bozhko_lab_3_2.databinding.FirstActivityBinding
 
 class FirstActivity : AppCompatActivity() {
-=======
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.bozhko_lab_3_2.databinding.FirstActivityBinding
-
-class FirstActivity : AppCompatActivity() {
-
->>>>>>> Stashed changes
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
--- app/src/main/java/com/example/bozhko_lab_3_2/MainActivity.kt
+++ app/src/main/java/com/example/bozhko_lab_3_2/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.example.bozhko_lab_3_2
 
-<<<<<<< Updated upstream
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-=======
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
->>>>>>> Stashed changes
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-<<<<<<< Updated upstream
-=======
-import androidx.navigation.ui.R
->>>>>>> Stashed changes
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -22,51 +14,37 @@ import com.example.bozhko_lab_3_2.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-<<<<<<< Updated upstream
-    private lateinit var appBarConfiguration: AppBarConfiguration
-=======
-
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
->>>>>>> Stashed changes
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-<<<<<<< Updated upstream
-        val binding = ActivityMainBinding.inflate(this.layoutInflater)
-=======
-        binding = ActivityMainBinding.inflate(layoutInflater)
->>>>>>> Stashed changes
+        binding = ActivityMainBinding.inflate(this.layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-<<<<<<< Updated upstream
                 R.id.nav_first, R.id.nav_second, R.id.nav_third
-=======
-                R.id.nav_firstActivity, R.id.nav_secondActivity, R.id.nav_thirdActivity
->>>>>>> Stashed changes
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-<<<<<<< Updated upstream
-=======
     }
 
     override fun onPause() {
         binding.drawerLayout.close()
         super.onPause()
->>>>>>> Stashed changes
     }
 
     override fun onSupportNavigateUp(): Boolean {
